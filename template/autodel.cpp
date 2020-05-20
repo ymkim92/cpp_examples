55061921a3e51e8af13cfe6acfa8d7137efe74d0
Youngmin Kim <ykim@topcon.com>
2020-05-20 12:52:15 +1000

    Add template

---
+++ template/autodel.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+template <class T>
+class Auto_ptr1
+{
+    T* m_ptr;
+public:
+    Auto_ptr1(T* ptr=nullptr)
+        :m_ptr(ptr)
+    {
+    }
+
+    ~Auto_ptr1()
+    {
+        delete m_ptr;
+    }
+
+    T& operator*() const {return *m_ptr;}
+    T* operator->() const {return m_ptr;}
+};
+
+class Resource
+{
+public:
+    Resource() {std::cout << "Resource acquired\n";}
+    ~Resource() {std::cout << "Resource destroyed\n";}
+};
+
+int main()
+{
+    Auto_ptr1<Resource> res(new Resource());
+
+    return 0;
+}
