6731f40d2b92e04e2178115f40c7c59c1d05df1c
Youngmin Kim <ykim@topcon.com>
2020-07-06 08:37:30 +1000

    Add rvalueref.cpp

--- string/convert.cpp
+++ string/convert.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
 #include <string>
 #include <string_view>
+#include <cstring> // For std::strlen
  
 void print(std::string s)
 {
   std::cout << s << '\n';
 }
- 
+
 int main()
 {
-  std::string_view sv{ "balloon" };
-  sv.remove_suffix(3);
- 
-  // print(sv); // compile error: won't implicitly convert
- 
-  std::string str{ sv }; // explicit conversion
-  print(str); // okay
-  print(static_cast<std::string>(sv)); // okay
-	   
-  return 0;
+    std::string_view sv{"balloon"};
+    sv.remove_suffix(3);
+
+    // print(sv); // compile error: won't implicitly convert
+
+    std::string str_s{sv};                 // explicit conversion
+    print(str_s);                          // okay
+    print(static_cast<std::string>(sv)); // okay
+    std::cout << std::strlen(str_s.c_str()) << '\n';
+
+    std::string_view str{"balloon"};
+
+    std::cout << str << '\n';
+
+    // We use std::strlen because it's simple, this could be any other function
+    // that needs a null-terminated string.
+    // It's okay to use data() because we haven't modified the view, and the
+    // string is null-terminated.
+    std::cout << std::strlen(str.data()) << '\n';
+
+    return 0;
 }
