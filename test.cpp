ae91dbf87bfbb74f198b39094b1d78954acbf2f1
Youngmin Kim <ykim@topcon.com>
2020-07-23 13:42:44 +1000

    Update

--- test.cpp
+++ test.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 
+// namespace data 
+// {
+//     typedef Acceleration float;
+// }
+
 int globalTestInt;
-vaoid Func()
+void Func(int intFunc)
 {
+    intFunc = 0;
+}
+
+void Func2(data::Acceleration &intFunc)
+{
+    intFunc = 0;
+}
+
+void Func3(uint32_t intFunc)
+{
+    intFunc = 0;
 }
 
 int main()
 {
     int testInteger;
 
-    testIn
-    globalTestInt
+    testInteger = 0;
+    globalTestInt = 1;
+    testI
     Fu
 }
