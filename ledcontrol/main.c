4a5c50cc3ea0cc3be6e852d8c65d7ad032a5586e
Youngmin Kim <ykim@topcon.com>
2020-09-18 12:52:59 +1000

    make it work for ledgui2.c

--- ledcontrol/main.c
+++ ledcontrol/main.c
@@ -44,13 +44,11 @@ int main()
     //     perror("sem_init");
     //     exit(EXIT_FAILURE);
     // }
-    printf("Before Thread\n"); 
     pthread_create(&thread_id3, NULL, LedControllerTask, NULL); 
     pthread_create(&thread_id, NULL, LedThread, NULL); 
     // pthread_create(&thread_id2, NULL, Thread2, NULL); 
     // pthread_join(thread_id, NULL); 
     // pthread_join(thread_id2, NULL); 
     pthread_join(thread_id3, NULL); 
-    printf("Never Ends\n"); 
     exit(0); 
 }
