2edeb51a578bf099ec5cf8c72e4d159851875681
Youngmin Kim <ymkim92@gmail.com>
2023-07-15 14:30:58 +1000

    add struct

---
+++ struct/struct_pod.cpp
@@ -0,0 +1,22 @@
+// Listing 4-22: A code segment illustrating structured binding declaration
+struct Result
+{
+    HumptyDumpty hd;
+    bool success;
+};
+Result make_humpty()
+{
+    HumptyDumpty hd{};
+    bool is_valid;
+    // Check that hd is valid and set is_valid appropriately
+    return {hd, is_valid};
+}
+bool send_kings_horses_and_men()
+{
+    auto [hd, success] = make_humpty();
+    if (!success)
+        return false;
+    // Class invariants established
+    // --snip-- 
+    return true;
+}
