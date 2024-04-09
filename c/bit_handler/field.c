2fc0b72b42aad184212cdf1187ec86d737f33415
Youngmin Kim <y.kim@skybornetech.com>
2024-04-09 16:22:57 +1000

    update

--- c/bit_handler/field.c
+++ c/bit_handler/field.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <assert.h>
 
 #define SERVO_HW_ERROR_SHIFT        7
 #define SERVO_HW_ERROR_MASK         (1<<SERVO_HW_ERROR_SHIFT)
@@ -14,9 +15,18 @@ uint32_t SetField(uint32_t all, uint32_t mask, uint32_t shift, uint32_t value)
     return (all & ~mask) | ((value << shift) & mask);
 }
 
+uint32_t SetBit(uint32_t all, uint32_t shift, uint32_t value)
+{
+    return SetField(all, 1 << shift, shift, value);
+}
+
 int main()
 {
     uint8_t err = 0x80;
     printf("%d \n", GetField(err, SERVO_HW_ERROR_MASK, SERVO_HW_ERROR_SHIFT));
+    uint32_t testAll = 0;
+    testAll = SetBit(testAll, 2, 1);
+    printf("testAll 0x%x\n", testAll);
+    assert(testAll == 4);
     return 0;
 }
